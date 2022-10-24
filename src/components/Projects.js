09be279f94e4c7897be56c69e3579cecefb14e74
Lukas Gonzalez <lukegonz47@gmail.com>
2022-10-23 21:16:55 -0400

    added logo and changed title, changed a tags to Link tags

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import scorecard from '../images/scorecard-img.png';
 import gymbot from '../images/gymbot-img.png';
 import vicepong from '../images/vicepong-img.png';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Projects() {
     return (
@@ -10,9 +10,9 @@ function Projects() {
                 <h1 className='projects-title'>ScoreCard</h1>
                 <img className='project-imgs' src={scorecard}/>
                 <div className='button-div'>
-                    <a target='_blank' href='https://elegant-centaur-2ad7d4.netlify.app/'>
-                        <button className='pro-buttons'>View Project</button>
-                    </a>
+                    <Link className='butt-link'>
+                        <button className='pro-buttons'>View Project</button>  
+                    </Link> 
                 </div>
                 
             </div>
@@ -20,9 +20,9 @@ function Projects() {
                 <h1 className='projects-title'>Gym Bot</h1>
                 <img className='project-imgs' src={gymbot}/>
                 <div className='button-div'>
-                    <a target='_blank' href='https://gym-bot-app.herokuapp.com/'>
-                        <button className='pro-buttons'>View Project</button>
-                    </a>
+                    <Link className='butt-link'>
+                        <button className='pro-buttons'>View Project</button>  
+                    </Link>              
                 </div>
                 
             </div>
@@ -30,9 +30,9 @@ function Projects() {
                 <h1 className='projects-title'>Vice Pong</h1>
                 <img className='project-imgs' src={vicepong}/>
                 <div className='button-div'>
-                    <a target='_blank' href='https://lukag7.github.io/Game-Project/'>
-                        <button className='pro-buttons'>View Project</button>
-                    </a>
+                    <Link className='butt-link'>
+                        <button className='pro-buttons'>View Project</button>  
+                    </Link> 
                 </div>
             </div>
         </div>
