0d11c6961527cba5bfd74ba3771872eb02cb63ca
Lukas Gonzalez <lukegonz47@gmail.com>
2022-10-31 23:05:56 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
-import NavBar from './components/Navbar';
 import About from './components/About';
 import ContactMe from './components/ContactMe';
 import HomePage from './components/HomePage';
@@ -9,7 +8,6 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App">
-      {/* <NavBar /> */}
       <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
