19cdb538e24c803fbf8562adbc9cbaa91d80fa0b
Lukas Gonzalez <lukegonz47@gmail.com>
2022-11-05 13:26:47 -0400

    added particles package, added p tag in homepage comp, styled homepage a bit

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,12 @@
-
-
 function HomePage() {
     return (
         <div className="homepage">
             <div className="name-fs">
                 <h1 className="name-h1">Lukas Gonzalez</h1>
                 <p className="name-p">Full Stack Web Developer</p>
+            </div>
+            <div className="p-div">
+                <p>A lifelong learner excited to absorb new ideas, and make an impact on the development of tomorrow.</p>
             </div>
         </div>
     )
